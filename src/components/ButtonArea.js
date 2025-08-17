f58b7cf01501389fe90c42737df7deb29a8bcd6e
hustler78237 <kumarrohit78237@gamil.com>
2025-08-17 13:12:35 +0530

    Add Changes to Src

--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -1,10 +1,10 @@
 function ButtonArea (){
     return (
         <div className="flex justify-center gap-4">
-           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">UpperCase</button>
-           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">LowerCase</button>
-           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">download</button>
-           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">Copy</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 ">UpperCase</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 ">LowerCase</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 ">download</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 ">Copy</button>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,16 @@ import { useState } from "react";
 function Navbar() {
 
     let [active, setActive] = useState(0);
+    const [open, setOpen] = useState(false);
 
     return (
         <form>
             <div className="flex flex-col items-center box-border  text-[#ffffff]  h-dvh  cursor-pointer">
-                <p onClick={() => setActive(active = 1)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2  rounded-lg   ${active === 1 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Textarea</p>
-                <p onClick={() => setActive(active = 2)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 2 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""} `}>History</p>
-                <p onClick={() => setActive(active = 3)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 3 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Login</p>
-                <p onClick={() => setActive(active = 4)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 4 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Sign Up</p>
-                <button className="mt-auto bg-gradient-to-r from-[#2a1452] to-[#4a128c] mb-4 px-4 py-2 rounded-lg">LogOut</button>
+                <p onClick={() => setActive(active = 1)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2  rounded-lg transition active:scale-95   ${active === 1 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Textarea</p>
+                <p onClick={() => setActive(active = 2)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg  transition active:scale-95  ${active === 2 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""} `}>History</p>
+                <p onClick={() => setActive(active = 3)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg  transition active:scale-95  ${active === 3 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Login</p>
+                <p onClick={() => setActive(active = 4)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg transition active:scale-95  ${active === 4 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Sign Up</p>
+                <button className="mt-auto bg-gradient-to-r from-[#2a1452] to-[#4a128c] mb-4 px-4 py-2 rounded-lg transition active:scale-95 ">LogOut</button>
             </div>
         </form>
     );
