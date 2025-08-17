bdab09a653738781415daaac627605844ab256a7
hustler78237 <kumarrohit78237@gamil.com>
2025-08-17 10:39:28 +0530

    Save my changes in src

--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -1,7 +1,10 @@
 function ButtonArea (){
     return (
-        <div>
-            this is button area
+        <div className="flex justify-center gap-4">
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">UpperCase</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">LowerCase</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">download</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg">Copy</button>
         </div>
     )
 }
