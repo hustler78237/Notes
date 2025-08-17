190ecbfe5ccfdf10b4245f40f7b7fefc24967b7a
hustler78237 <kumarrohit78237@gamil.com>
2025-08-17 08:43:01 +0530

    Add Changes in my Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
-function Navbar () {
+function Navbar() {
 
-    let [active , setActive] = useState(0);
+    let [active, setActive] = useState(0);
 
-    return(
+    return (
         <form>
             <div className="flex flex-col items-center box-border bg-[#14152a] text-[#ffffff]  h-dvh  cursor-pointer">
-                <p onClick={()=> setActive(active = 1)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2  rounded   ${active === 1 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center":""}`}>Textarea</p>
-                <p onClick ={()=> setActive(active = 2)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded ${active === 2 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center":""} `}>History</p>
-                <p onClick={() =>setActive(active = 3)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2  ${active === 3 ?"bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center":""}`}>Login</p>
-                <p onClick={() => setActive(active = 4)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 ${active === 4 ?"bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center":""}`}>Sign Up</p>
-                <button className="mt-auto">LogOut</button>
+                <p onClick={() => setActive(active = 1)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2  rounded-lg   ${active === 1 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Textarea</p>
+                <p onClick={() => setActive(active = 2)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 2 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""} `}>History</p>
+                <p onClick={() => setActive(active = 3)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 3 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Login</p>
+                <p onClick={() => setActive(active = 4)} className={`hover:bg-gradient-to-r from-[#2a1452] to-[#4a128c] hover:px-4 py-2 rounded-lg ${active === 4 ? "bg-gradient-to-r from-[#2a1452] to-[#4a128c] w-full text-center" : ""}`}>Sign Up</p>
+                <button className="mt-auto bg-gradient-to-r from-[#2a1452] to-[#4a128c] mb-4 px-4 py-2 rounded-lg">LogOut</button>
             </div>
         </form>
     );
