172f410f205b50db516019a14d51e6dcf62fb048
hustler78237 <kumarrohit78237@gamil.com>
2025-08-23 11:38:02 +0530

    Add my changes to src

--- src/parentcopycomponents/Login.js
+++ src/parentcopycomponents/Login.js
@@ -1,7 +1,11 @@
-function Login (){
+import Navbar from "../components/Navbar";
+
+function Login() {
     return (
-        <div>
-            hello this is a Login page
+        <div className="flex flex-col h-dvh md:flex-row ">
+            <div className="md:basis-1/4  bg-[#14152a] ">
+                <Navbar />
+            </div>
         </div>
     );
 }
