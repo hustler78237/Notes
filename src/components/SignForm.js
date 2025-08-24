0116c0d24b85ec48efe5b2ca94b8ffb44a9b3cf4
hustler78237 <kumarrohit78237@gamil.com>
2025-08-24 12:28:17 +0530

    Add my changes to src

--- src/components/SignForm.js
+++ src/components/SignForm.js
@@ -2,6 +2,10 @@ function SignForm() {
     return (
         <div className="flex items-center justify-center min-h-screen">
             <form className="flex flex-col justify-center bg-[#14152a] p-4 rounded-lg shadow-2xl object-contain ">
+                <label>Name</label>
+                <div>
+                    <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Name"></input>
+                </div>
                 <label>Username</label>
                 <div>
                     <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Username"></input>
@@ -13,6 +17,10 @@ function SignForm() {
                 <label>Password</label>
                 <div>
                     <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Password"></input>
+                </div>
+                <label>Confirm Password</label>
+                <div>
+                    <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Confirm Password"></input>
                 </div>
                 <button className="bg-gradient-to-r from-[#2a1452] to-[#4a128c]
                         px-4 py-2 rounded-lg transition active:scale-95 mt-3 
