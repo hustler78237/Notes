bb20631a44fe9c1791dab37852309bae7fa850e0
hustler78237 <kumarrohit78237@gamil.com>
2025-08-24 12:06:49 +0530

    Add my changes to my src app

---
+++ src/components/LogForm.js
@@ -0,0 +1,25 @@
+function LogForm() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <form className="flex flex-col justify-center bg-[#14152a] p-4 rounded-lg shadow-2xl object-contain ">
+                <label>Username</label>
+                <div>
+                    <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Username"></input>
+                </div>
+                <label>Email</label>
+                <div>
+                    <input type="email" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Email"></input>
+                </div>
+                <label>Password</label>
+                <div>
+                    <input type="text" className="rounded-lg text-black p-1 w-80 focus:border-white focus:ring-4 focus:ring-indigo-600 focus:outline-none font-medium" placeholder="Password"></input>
+                </div>
+                <button className="bg-gradient-to-r from-[#2a1452] to-[#4a128c]
+                        px-4 py-2 rounded-lg transition active:scale-95 mt-3 
+                         ">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LogForm;
--- src/parentcopycomponents/Login.js
+++ src/parentcopycomponents/Login.js
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
+import LogForm from "../components/LogForm";
 
 function Login() {
-    return (
+    return ( 
         <div className="flex flex-col h-dvh md:flex-row ">
             <div className="md:basis-1/4  bg-[#14152a] ">
                 <Navbar />
+            </div>
+            <div className="flex flex-col flex-1 ">
+                <div className="flex-1 bg-[#0d111c] overflow-auto text-[#ffffff]">
+                    <LogForm />
+                </div>
             </div>
         </div>
     );
