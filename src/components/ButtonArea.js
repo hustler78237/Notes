116922a8f0aa9a4ad7992ca634da33634b04d18d
hustler78237 <kumarrohit78237@gamil.com>
2025-08-30 14:32:15 +0530

    add my changes to src

--- src/components/ButtonArea.js
+++ src/components/ButtonArea.js
@@ -22,12 +22,22 @@ function ButtonArea ({text, setText, title }){
         URL.revokeObjectURL(url);
     }
 
+
+    function copy(){
+        navigator.clipboard.writeText(text)
+        .then(()=>{
+            alert("Text copied to Clipboard");
+        })
+        .catch(err => {
+            console.error("Failed to Copy: ", err);
+        })
+    }
     return (
         <div className="flex flex-wrap justify-center gap-2 sm:gap-4">
            <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 text-sm sm:text-base " onClick={()=>{UpperCase()}}>UpperCase</button>
            <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 text-sm sm:text-base " onClick={()=> {LowerCase()}}>LowerCase</button>
            <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 text-sm sm:text-base "onClick={()=>{handleDownload()}}>download</button>
-           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 text-sm sm:text-base ">Copy</button>
+           <button className=" bg-gradient-to-r from-[#2a1452] to-[#4a128c]  px-4 py-2 rounded-lg transition active:scale-95 text-sm sm:text-base " onClick={()=>{copy()}}>Copy</button>
         </div>
     )
 }
