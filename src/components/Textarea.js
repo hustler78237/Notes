3201e9cc71504539de2582932eae3f5975e7d082
hustler78237 <kumarrohit78237@gamil.com>
2025-08-30 12:20:17 +0530

    add my changes in textarea

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Textarea() {
+
+  // this logic for storing title in localStorage of browser
+  const[title, setTitle] = useState(()=>{
+    let savedTile = localStorage.getItem("Title");
+    return savedTile ? JSON.parse(savedTile) : "";
+  })
+  // this logic for storing textarea in localStorage of browser
+  const [textarea , setTextarea] = useState(() => {
+    let savedText = localStorage.getItem("text");
+    return savedText ? JSON.parse(savedText) : "";
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("Title", JSON.stringify(title));
+  },[title])
+  
+  useEffect(()=>{
+    localStorage.setItem("text" , JSON.stringify(textarea))
+  },[textarea])
+  
+
+
     return (
         <div className="flex flex-col items-center gap-4 p-4 h-full">
       {/* Input heading */}
@@ -6,12 +30,16 @@ function Textarea() {
         type="text"
         className="w-full max-w-lg h-10 bg-[#14152a] font-semibold text-[#ffffff] text-center focus:border-white focus:ring-2 focus:ring-indigo-600 focus:outline-none shadow-2xl rounded-lg"
         placeholder="Enter Your Note Heading"
+        value={title}
+        onChange={(e)=> setTitle(e.target.value)}
       />
 
       {/* Textarea auto-fits container */}
       <textarea
         className="w-full flex-1 bg-[#14152a] text-[#ffffff] focus:border-white focus:ring-2 focus:ring-indigo-600 focus:outline-none shadow-2xl rounded-lg p-4 resize-none"
         placeholder="Enter your Note here"
+        value={textarea}
+        onChange={(e)=> setTextarea(e.target.value)}
       ></textarea>
     </div>
     )
